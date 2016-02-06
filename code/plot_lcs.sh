f8cfe27258033b73d1d7ea0db003180d3db8ac28
Toon Nolten <toon.nolten@student.kuleuven.be>
2016-02-06 11:02:51 +0100

    plot_lcs on BD, zoo gives too little data point (10 and 50)

--- code/plot_lcs.sh
+++ code/plot_lcs.sh
@@ -4,16 +4,18 @@
 TRAIN=../data/synthetic/bigdata.0.25.0.00.train
 TEST=../data/synthetic/bigdata.0.25.0.00.test
 
-TRAIN=../data/zoo-train.dat
-TEST=../data/zoo-test.dat
+#TRAIN=../data/zoo-train.dat
+#TEST=../data/zoo-test.dat
 
 # Generate LR data
 make LogisticRegression.class
+rm -f out.lr.acc
 java LogisticRegression 0.0001 $TRAIN $TEST out 10
 
 # Generate VFDT data
 make Vfdt.class
 make VfdtNode.class
+rm -f out.vfdt.acc
 java Vfdt 0.01 0.0001 $TRAIN $TEST out 10
 
 
